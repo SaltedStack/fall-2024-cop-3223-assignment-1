d0f3ed1d1558eb4f7779a24b849584110ac563df
SaltedStack <fonseca6422@gmail.com>
2024-09-08 22:57:42 +0000

    3rd commit

--- fracture.c
+++ fracture.c
@@ -2,82 +2,126 @@
 #include <math.h>
 #define PI 3.14159
 
-//by fabian fonseca - cop3223c
+//by fabian fonseca - UCFID 5166910
 
 //helper functions (UDF's meant to seperate logic into new functions)
-double calculateDistance();
-    {
-        //no args
-        double x1, y1, x2, y2, distance;
+void input(double *x1, double *y1, double *x2, double *y2) {
+    //double x1, y1, x2, y2, distance;
 
-        //point 1 input
-        printf("Please enter values for x1 and y1 respectively: ");
-        scanf("%lf %lf", &x1, &y1);
+    //point 1 input
+    printf("Please enter values for x1 and y1 respectively: ");
+    scanf("%lf %lf", x1, y1);
         
-        //point 2 input
-        printf("Please enter values for x2 and y2 respetively: ");
-        scanf("%lf %lf", &x2 &y2);
+    //point 2 input
+    printf("Please enter values for x2 and y2 respetively: ");
+    scanf("%lf %lf", x2, y2);
+}
+   
+void formula(double x1, double y1, double x2, double y2, double *distance, double *radius, double *perimeter, double *area) {
+    //dist formula
+    *distance = sqrt (pow(x2 - x1, 2)+ pow(y2 - y1, 2));
 
-        //distance (dist formula?)
-        distance = sqrt (pow(x2 - x1, 2)+ pow(y2 - y1, 2));
+    //perimeter formula
+    *radius = *distance / 2;
+    *perimeter = 2 * PI * (*radius);
 
-        //3rd line of text
-        printf("the distance between the two points is: ", distance);
+    //area formula
+    //radius = distance /2;
+    *area = PI * pow(*radius, 2);
+}
+
+double calculateDistance() { 
+    double x1, y1, x2, y2, distance, perimeter, radius, area;
+
+    //caling input
+    input(&x1, &y1, &x2, &y2);
+        
+    //dist formula
+    //distance = sqrt (pow(x2 - x1, 2)+ pow(y2 - y1, 2));
+    //calling formula
+    formula(x1, y1, x2, y2, &distance, &radius, &perimeter, &area);
+
+
+    //3rd line of text
+    printf("The distance between the two points is: %.2f\n", distance);
        
-        return distance;
-    }
+    return distance;
+}
 
-double calculatePerimeter();
-    {
-        //no args
+double calculatePerimeter() {
+    double x1, y1, x2, y2, distance, perimeter, radius, area;
 
-        //outs 2 lines of text
-            /*– Point #1 entered: x1 = -whatever was entered-; y1
-            = -whatever was entered-
-            – Point #2 entered: x2 = -whatever was entered-; y2
-            = -whatever was entered-
-            – The perimeter of the city encompassed by your re-
-            quest is -whatever is computed-*/
-        //returns a double how difficult this function was to create
-        //must use calculate distance to avoid repeating code
+    //call input
+    input(&x1, &y1, &x2, &y2);
+
+    //perimeter formula
+    //radius = distance / 2;
+    //perimeter = 2 * PI * radius;
+    //calling formula
+    formula(x1, y1, x2, y2, &distance, &radius, &perimeter, &area);
+    //3rd line of text
+    printf("The perimeter of the city encompassed by your request is: %2f\n", perimeter);
+
+    //difficulty
+    return 3;
+}
+
+double calculateArea() {
+    double x1, y1, x2, y2, distance, area, radius, perimeter;
+
+    //call input
+    input(&x1, &y1, &x2, &y2);
+
+    //calling formula
+    formula(x1, y1, x2, y2, &distance, &radius, &perimeter, &area);
+
+    //output
+    printf("The area of the city encompassed by your request is: %2f\n", area);
     
-    }
+    //difficulty
+    return 3;
+}
 
-double calculateArea();
-    {
-        /*• Arguments: This function have no arguments.
-        • Output: This function should output three lines of text
-        – Point #1 entered: x1 = -whatever was entered-; y1
-        = -whatever was entered-
-        – Point #2 entered: x2 = -whatever was entered-; y2
-        = -whatever was entered-
-        – The width of the city encompassed by your request
-        is -whatever is computed-
-        • Return: A double indicating how difficult you found to
-        do this function on a scale of 1.0 through 5.0 where 1 is
-        easy and 5 is hard*/
-    }
+double calculateWidth() {
+    double x1, y1, x2, y2, distance, area, radius, perimeter;
 
-double calculateHeight();
-    {
-        /*• Arguments: This function have no arguments.
-        • Output: This function should output three lines of text
-        – Point #1 entered: x1 = -whatever was entered-; y1
-        = -whatever was entered-
-        – Point #2 entered: x2 = -whatever was entered-; y2
-        = -whatever was entered-
-        – The height of the city encompassed by your request
-        is -whatever is computed-
-        • Return: A double indicating how difficult you found to
-        do this function on a scale of 1.0 through 5.0 where 1 is
-        easy and 5 is hard*/
-    }
+    //call input
+    input(&x1, &y1, &x2, &y2);
 
-//optional function
-//double askForUserInput();
-    {
-        //no args, no output, returns double representing the users typed in value, attributes to the DRY method
-    }
+    //calling formula
+    formula(x1, y1, x2, y2, &distance, &radius, &perimeter, &area);
+
+    //output
+    printf("The width of the city encompassed by your request is: %2f\n", distance);
+    
+    //difficulty
+    return 1;
+}
+
+double calculateHeight() {
+    double x1, y1, x2, y2, distance, area, radius, perimeter;
+
+    //call input
+    input(&x1, &y1, &x2, &y2);
+
+    //calling formula
+    formula(x1, y1, x2, y2, &distance, &radius, &perimeter, &area);
+
+    //output
+    printf("The height of the city encompassed by your request is: %2f\n", distance);
+    
+    //difficulty
+    return 1;
+}
+
+
+/*optional function
+double askForUserInput() {
+    double x1, y1, x2, y2
+    return x1, y1, x2, y2;
+}
+*/
+
 
 int main(int argc, char **argv) {
     calculateDistance () ;
